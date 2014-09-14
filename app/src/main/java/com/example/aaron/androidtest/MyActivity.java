a322d3e498c4515835b21cf5333568fbd738652c
Erns <aaron.sayles@gmail.com>
2014-09-14 12:44:07 -0700

    New layout xml added, commented out some bad code in myactivity.java

--- app/src/main/java/com/example/aaron/androidtest/MyActivity.java
+++ app/src/main/java/com/example/aaron/androidtest/MyActivity.java
@@ -60,15 +60,19 @@ public class MyActivity extends Activity
         switch (number) {
             case 1:
                 mTitle = getString(R.string.title_section1);
+                //setContentView(R.layout.activity_my);
                 break;
             case 2:
                 mTitle = getString(R.string.title_section2);
+                //setContentView(R.layout.section2);
                 break;
             case 3:
                 mTitle = getString(R.string.title_section3);
+                //setContentView(R.layout.activity_my);
                 break;
             case 4:
                 mTitle = "Working!!";
+               // setContentView(R.layout.section2);
                 break;
         }
     }
